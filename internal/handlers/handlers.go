4e2a347b571f0f1e865a2cb86b9eec256d606ee2
SytnikDima <SytnikDima@gmail.com>
2025-05-20 17:04:15 +0300

    update3

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -66,6 +66,6 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//Устанавливаем заголовок, как результат изменений
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Write([]byte(output))
 }
